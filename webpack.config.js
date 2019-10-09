d48c3e9c3d3fc7ea9116c3d1a784994df5bac166
qburnsFB <quinton@giddy.io>
2019-10-09 12:50:10 -0400

    switch to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'dist'),
+        library: 'linaria-css-grid',
+        libraryTarget: 'umd'
     },
+    externals: {
+        linaria: {
+            commonjs: 'linaria',
+            commonjs2: 'linaria',
+            amd: 'linaria',
+            root: 'linaria'
+        }
+    }
 };
